457293b400efd3565017caccfd7c3cd7d00b4bbc
Rory-luo <55289804+Rory-luo@users.noreply.github.com>
2022-09-01 07:25:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ def get_period_left():
     if next_time < nowtime < next_time_period:
         words_reply = "今天是小馋猫例假来的第{0}天".format((today - next_time).days + 1)
         return words_reply
-    if next_time.days > nowtime.days:
+    if next_time.day > nowtime.day:
         # next_time = next_time.replace(month=next_time.month + 1)
         words_reply = "距离小馋猫的例假来临还有{0}天".format((next_time - today).days + 1)
         return words_reply
